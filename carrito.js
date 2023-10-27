52b0e421e8997295051d056b714c37c078b1084c
Manufalchettoni <manufalchettoni@gmail.com>
2023-10-27 16:58:38 -0300

    Documentacion de archivos js

--- carrito.js
+++ carrito.js
@@ -1,15 +1,17 @@
+//Recuperamos el carrito del localStorage
 const carrito = JSON.parse(localStorage.getItem('carrito'));
 
+//Esperamos que cargue el DOM
 document.addEventListener('DOMContentLoaded', function() {
 
-  // Mostrar productos del carrito----------
+  // Buscamos los elementos en HTML
   const contenedorCarrito = document.getElementById('carrito-contenedor');
   const totalElement = document.getElementById('total-compra');
-  //Mostrar productos en HTML
+  //Buscamos el contenedor
   if (contenedorCarrito) {
     // Itera sobre los productos
     carrito.productos.forEach(producto => {
-      // Crea un elemento 
+      // Crea un elemento nuevo HTML
       const productoElement = document.createElement('div');
       productoElement.innerHTML = `
         <h2>${producto.nombre}</h2>
@@ -21,19 +23,20 @@ document.addEventListener('DOMContentLoaded', function() {
       // Agrega el elemento del producto
       contenedorCarrito.appendChild(productoElement);
     });
-    totalElement.textContent = `Total: $${carrito.total.toFixed(2)}`;
+    // Muestra el precio total
+    totalElement.textContent = `Total: $${carrito.total.toFixed(2)}`;//Solo mostramos dos decimales
   }
 
 
 //Simular pago--------------
 function realizarPago() {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {//Se crea una nueva promesa
     // Simular el proceso de pago
-    setTimeout(() => {
-      const exito = Math.random() < 0.8; // Simula si el pago fue exitoso
-      if (exito) {
+    setTimeout(() => {//Para simular el proceso de pago con una demora 
+      const exito = Math.random() < 0.8; //Se genera un número aleatorio y se compara con 0.8, esto simula si el pago fue exitoso o no
+      if (exito) {//Si el numero es menor a 0.8
         resolve("¡Compra exitosa! Su pedido se ha procesado.");
-      } else {
+      } else {//Si el numero es mayor a 0.8
         reject("El pago ha fallado. Por favor, inténtelo de nuevo.");
       }
     }, 2000); // Simula una demora de 2 segundos 
@@ -42,6 +45,7 @@ function realizarPago() {
 
 // Evento en boton de comprar--------------
 const comprarBtn = document.getElementById('comprar-btn');
+//Agrega un evento al boton
 comprarBtn.addEventListener('click', () => {
   realizarPago()
     .then(mensaje => {
@@ -54,10 +58,11 @@ comprarBtn.addEventListener('click', () => {
 
 
 //Eliminar del carrito ---------------
+//Busco los botones de eliminar
 const eliminarBotones = document.querySelectorAll('.eliminar-producto');
 
   eliminarBotones.forEach(boton => {
-    boton.addEventListener('click', function() {
+    boton.addEventListener('click', function() { //Se ejecuta una funcion cuando un usuario haga clic
       const productoElement = this.parentNode; // Elemento del producto
       const nombreProducto = productoElement.querySelector('h2').textContent; // Nombre del producto
 
@@ -71,12 +76,14 @@ const eliminarBotones = document.querySelectorAll('.eliminar-producto');
     });
   });
   function eliminarProductoDelCarrito(nombreProducto) {
+    //Buscamos el producto en el carrito
     const indice = carrito.productos.findIndex(producto => producto.nombre === nombreProducto);
+    //Si lo encontramos, lo elimino; si no lo encotramos el indice seria -1;
     if (indice !== -1) {
-      const precioProductoEliminado = carrito.productos[indice].precio;
-      carrito.total -= precioProductoEliminado;
-      carrito.productos.splice(indice, 1);
-      totalElement.textContent = `Total: $${carrito.total.toFixed(2)}`;
+      const precioProductoEliminado = carrito.productos[indice].precio;//Se busca el precio del producto
+      carrito.total -= precioProductoEliminado;//Se resta del total el precio del producto
+      carrito.productos.splice(indice, 1);//Se elimina un elemento en la posicion indicada
+      totalElement.textContent = `Total: $${carrito.total.toFixed(2)}`;//Se redondea a 2 decimales el total
     }
   }
   
--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 let data;
 /* Productos */
-fetch('productos.json')
-  .then(response => response.json())
-  .then(dataResponse => {
+fetch('productos.json')//Realizamos una solicitud al archivo JSON
+  .then(response => response.json())//Convierte el contenido JSON en un objeto JavaScript
+  .then(dataResponse => {  //se asigna el objeto JSON a la variable dataResponse
     data = dataResponse
-    const listaProductos = document.getElementById('lista-productos');
+    const listaProductos = document.getElementById('lista-productos');//Se busca el contenedor en el HTML
 
-    data.productos.forEach(producto => {
+    data.productos.forEach(producto => { //Se recorren todos los productos
       // Crea elemento HTML 
       const productoElement = document.createElement('div');
-      productoElement.innerHTML = `
+      //Se especifica el cotenido nuevo del elemnto HTML
+      productoElement.innerHTML = ` 
         <h2><a href="productos/producto${producto.id}.html">${producto.nombre}</a></h2>
         <p>Precio: $${producto.precio}</p>
         <p>${producto.descripcion}</p>
@@ -22,39 +23,36 @@ fetch('productos.json')
     });
     
     //Boton para agregar al carrito
-    const botonesAgregarCarrito = document.querySelectorAll('.agregar-carrito');
+    const botonesAgregarCarrito = document.querySelectorAll('.agregar-carrito');//se seleccionan todos los elementos HTML que tienen la clase "agregar-carrito"
     botonesAgregarCarrito.forEach(boton => {
-      boton.addEventListener('click', function() {
-        const id = boton.getAttribute('data-id');
-        console.log(id)
-        console.log(carrito.productos)
-        agregarAlCarrito(Number(id));
+      boton.addEventListener('click', function() { //Se ejecuta una funcion cuando un usuario haga clic
+        const id = boton.getAttribute('data-id'); //se obtiene el "data-id" del botón en el que se hizo clic
+        agregarAlCarrito(Number(id));//Se llama  una funcion pasandole como argumento el identificador del boton
       });
     }); 
   })
-  .catch(error => {
+  .catch(error => { //Si ocurre un error al recuperar los datos
     console.error('Error al obtener los productos:', error);
   });
 
 
   /*Creación del Carrito */
   const carrito = {
-    productos: [],
-    total: 0,
-    agregarProducto: function(producto) {
-      this.productos.push(producto);
+    productos: [],//Creamos un array para guardas los "productos"
+    total: 0,//Iniciamos el precio total en cero
+    agregarProducto: function(producto) { //Se crea un metodo dentro del objeto que contiene un funcion
+      this.productos.push(producto);//"This" hace referencia a "carrito", luego se utiliza PUSH para agregar el producto(argumento) a productos(array) contenido en el carrito
     },
     
   };
 
   /* Agregar al carrito */
-  let totalCompra = 0;
    function agregarAlCarrito(id) {
-    const producto = data.productos.find(producto => producto.id === id);
-    if (producto) {
-      carrito.total += producto.precio;
-      carrito.agregarProducto(producto);
-      localStorage.setItem('carrito', JSON.stringify(carrito));
+    const producto = data.productos.find(producto => producto.id === id);//Se busca un producto en el objeto data.productos
+    if (producto) { //Se verifica si se encontró un producto
+      carrito.total += producto.precio;//Se aumenta el valor del total del carrito sumando el precio del producto
+      carrito.agregarProducto(producto);//Metodo del objeto carrito, pasando el producto encontrado como argumento
+      localStorage.setItem('carrito', JSON.stringify(carrito));//Se almacena el estado actual del carrito en el navegador
     }
   }
   
